668b3bde0bdf57e572c31628a048de5f6c76f67c
Amir Babaei <babaei.a94@gmail.com>
2019-05-06 20:35:45 -0700

    try 1

---
+++ lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.lab3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void download(View view) {
+        Intent intent = new Intent(MainActivity.this, download.class);
+        startActivity(intent);
+    }
+
+    public void delete(View view) {
+        Intent intent = new Intent(MainActivity.this, DeleteImage.class);
+        startActivity(intent);
+    }
+
+    public void VIEW(View view) {
+        Intent intent = new Intent(MainActivity.this, ViewImage.class);
+        startActivity(intent);
+    }
+
+    public void RANGE(View view) {
+        Intent intent = new Intent(MainActivity.this, Range.class);
+        startActivity(intent);
+    }
+}
