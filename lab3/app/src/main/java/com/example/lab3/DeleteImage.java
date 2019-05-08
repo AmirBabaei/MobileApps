600c1c1b2f9c6ef44f8e3098b33d4b08ae403b86
Amir <babaei.a94@gmail.com>
2019-05-08 13:34:55 -0700

    it downloads

--- lab3/app/src/main/java/com/example/lab3/DeleteImage.java
+++ lab3/app/src/main/java/com/example/lab3/DeleteImage.java
@@ -1,13 +1,22 @@
 package com.example.lab3;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DeleteImage extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_delete_image);
+
+
     }
+
 }
--- lab3/app/src/main/java/com/example/lab3/download.java
+++ lab3/app/src/main/java/com/example/lab3/download.java
@@ -1,13 +1,46 @@
 package com.example.lab3;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class download extends AppCompatActivity {
+
+
+    static TextView getUrl;
+    static ImageView imageView;
+
+    ConnectivityManager connectivityManager;
+    NetworkInfo networkInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
+
+        getUrl = findViewById(R.id.geturl);
+        imageView = findViewById(R.id.imageView);
+
+        //download image
+        connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        networkInfo = connectivityManager.getActiveNetworkInfo();
     }
+
+
+    public void downloadImage(View view) {
+        networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            ImageDownloader imageDownloader = new ImageDownloader();
+            imageDownloader.execute(getUrl.getText().toString());
+        }
+        else{
+            Toast.makeText(download.this, "NO INTERNET CONNECTION", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
